acc4ec98a769cf4da4ef532ad7ae5a793dd017ba
Gabriel Green Cube Games <ggbarbos86@gmail.com>
2020-10-30 03:31:33 -0300

    First Alpha Version of APK
    This version don't search for the file, only work with string inside unity, gonna upgrade later to make it open pdfs, txt, and some other text files to read, the next update will also bring a speed control, font control, and color of background and text...

---
+++ FastReader/Assets/Scripts/Reader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reader : MonoBehaviour
+{
+    [TextArea(3,80)]
+    public string fullText;
+    public int actualWord = 0;
+
+    public Text showWord;
+
+    public float speed;
+    float timer;
+    string word;
+
+    private void Start()
+    {
+        word = fullText.Split(' ')[actualWord];
+        timer = speed;
+    }
+
+    void Update()
+    {
+        if(speed > 0)
+        {
+            speed -= Time.deltaTime;
+        }
+        else
+        {
+            ShowNextWord();
+            speed = timer;
+        }
+        showWord.text = word;
+    }
+
+    public void ShowNextWord()
+    {
+        actualWord += 1;
+        word = fullText.Split(' ')[actualWord];
+    }
+}
